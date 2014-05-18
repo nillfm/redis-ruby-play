dbeb5f36ed03e7ded8707d3e50967c0be6779a58
Frederick Myers <fmyers@leafsoftwaresolutions.com>
2014-05-18 19:50:15 -0400

    simple web notification with socket.io, node.js, and redis

--- README.md
+++ README.md
@@ -4,5 +4,14 @@ redis-ruby-play
 Just playing with redis and ruby. Nothing to see here!
 
 Simple sets: `simple-set.rb`
+
 Pub/sub: `pub.rb`, `sub.rb`
+
 Keyspace pub/sub: `keyspace-pub.rb`, `keyspace-sub.rb`
+
+Simple web client notification using redis, socket.io, and node.js: `node-web-notify`
+# `cd node-web-notify`
+# `npm install`
+# `node app.js`
+# In browser, open up localhost:3000
+# In another term, `redis-cli public notify-fifo hello`
---
+++ node-web-notify/app.js
@@ -0,0 +1,32 @@
+var  express = require('express')
+  ,  http = require('http')
+  ,  fs = require('fs')
+  ,  redis = require('redis');
+
+var app = express();
+var server = app.listen(3000);
+var io = require('socket.io').listen(server);
+
+app.configure(function(){
+  app.use(express.logger('dev'));
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+});
+
+app.configure('development', function() {
+  app.use(express.errorHandler());
+});
+
+io.sockets.on('connection', function(socket) {
+  const redisClient = redis.createClient();
+  redisClient.subscribe('notify-fifo');
+
+  redisClient.on("message", function(channel, message) {
+    socket.send(message);
+  });
+});
+
+
+console.log("Server up and running!");
